247ad3ba1fdaf564936d3d44e48b68c9102d402b
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-15 10:41:32 -0700

    0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t nodes = 0;
+	size_t counter_t = 0;
 
-	if (!h)
-		return (0);
-
-	while (h)
+	while (h != NULL)
 	{
-		printf("%d\n", h->n);
+		printf("%i\n", h->n);
 		h = h->next;
-		nodes++;
+		counter_t++;
 	}
-
-	return (nodes);
+	return (counter_t);
 }
