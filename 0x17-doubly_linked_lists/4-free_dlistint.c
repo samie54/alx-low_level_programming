68640844da56ec77f9b44ae54743802b23607613
samie54 <126026530+samie54@users.noreply.github.com>
2023-06-15 10:13:16 -0700

    4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,1 +1,17 @@
-dsdf
+#include "lists.h"
+
+/**
+ * free_dlistint - frees doubly linked dlist.
+ * @head: pointer to list to be freed.
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+}
