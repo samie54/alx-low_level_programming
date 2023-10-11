313919b30cb8f59978dba307908fc391dc996758
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-11 11:05:32 -0700

    MyHeaderFile

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,36 +1,13 @@
 #ifndef SEARCH_ALGOS
 #define SEARCH_ALGOS
-
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * struct listint_s - singly linked list
- *
- * @n: integer
- * @index: Index of the node in the list
- * @next: Pointer to next node
- *
- * Description: sinlgy linked list node struct
- */
-typedef struct listint_s
-{
-	int n;
-	size_t index;
-	struct listint_s *next;
-} listint_t;
 
-/**
- * struct skiplist_s - Singly linked list with express lane
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- *
- * Description: singly linked list node struct with an express lane
- */
+/* MyHelpFiles */
+void print_array(int *array, size_t inc, size_t size);
+
 typedef struct skiplist_s
 {
 	int n;
@@ -39,13 +16,23 @@ typedef struct skiplist_s
 	struct skiplist_s *express;
 } skiplist_t;
 
-int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t size, int value);
-int jump_search(int *array, size_t size, int value);
-int interpolation_search(int *array, size_t size, int value);
-int exponential_search(int *array, size_t size, int value);
-int advanced_binary(int *array, size_t size, int value);
-listint_t *jump_list(listint_t *list, size_t size, int value);
-skiplist_t *linear_skip(listint_t *list, int value);
+typedef struct listint_s
+{
+	int n;
+	size_t index;
+	struct listint_s *next;
+} listint_t;
 
-#endif /* SEARCH_ALGOS*/
+/* MyMainFunctions */
+int jump_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int advanced_binary(int *array, size_t size, int value);
+int exponential_search(int *array, size_t size, int value);
+
+skiplist_t *linear_skip(skiplist_t *list, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+
+
+#endif
