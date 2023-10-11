46a0649bfb758501979880eb43212e1dd37e6e9c
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-11 14:33:08 -0700

    Task 12

--- 0x1E-search_algorithms/105-jump_list.c
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -1,11 +1,17 @@
 #include "search_algos.h"
 
 /**
- * linear_skip - linear skip search
- * @list: Pointer to head of linked list
- * @value: Search value
+ * jump_list - Searches for an algorithm in a sorted singly
+ *             linked list of integers using jump search.
+ * @list: A pointer to the  head of the linked list to search.
+ * @size: The number of nodes in the list.
+ * @value: The value to search for.
  *
- * Return: Pointer to first node of value; hence NULL
+ * Return: If the value is not present or the head of the list is NULL, NULL.
+ *         Otherwise, a pointer to the first node where the value is located.
+ *
+ * Description: Prints a value every time it is compared in the list.
+ *              Uses the square root of the list size as the jump step.
  */
 listint_t *jump_list(listint_t *list, size_t size, int value)
 {
