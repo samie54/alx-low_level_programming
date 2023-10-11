cf1cb1b19c51607dfa0a88e6bf109ebd9761ea04
samie54 <126026530+samie54@users.noreply.github.com>
2023-10-11 14:12:15 -0700

    Task 13

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,1 +1,46 @@
 #include "search_algos.h"
+
+/**
+ * linear_skip - Searches in sorted singly
+ * linked list of integers
+ * @list: Pointerr to head of linked list
+ * @value: Tsearch value
+ *
+ * Return: NULL if value  not present; or head of list is NULL
+ * return pointerr to first nodeof value
+ *
+ * Description: Prints value every time it's compared in list
+ * Makes use of square root of list size
+ */
+skiplist_t *linear_skip(skiplist_t *list, int value)
+{
+	skiplist_t *node, *jump;
+
+	if (list == NULL)
+		return (NULL);
+
+	for (node = jump = list; jump->next != NULL && jump->n < value;)
+	{
+		node = jump;
+		if (jump->express != NULL)
+		{
+			jump = jump->express;
+			printf("Value checked at index [%ld] = [%d]\n",
+					jump->index, jump->n);
+		}
+		else
+		{
+			while (jump->next != NULL)
+				jump = jump->next;
+		}
+	}
+
+	printf("Value found between indexes [%ld] and [%ld]\n",
+			node->index, jump->index);
+
+	for (; node->index < jump->index && node->n < value; node = node->next)
+		printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+	printf("Value checked at index [%ld] = [%d]\n", node->index, node->n);
+
+	return (node->n == value ? node : NULL);
+}
